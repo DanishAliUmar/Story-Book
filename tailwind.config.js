a9e3c15b7ff532c8c390e4ced11e6f82ab617136
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-08-08 15:04:59 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      Primary: '#FFA732',
+      color_Black: '#202020',
+      White: '#FFFFFF',
+      Button_Color: '#E1000A',
+      Shapes_on_primary: '#FFB758',
+      Error_State: '#FF0707',
+      Active_State: '#27C100',
+      Grey_1: '#797979',
+      Grey_2: '#D2D2D2',
+      Error_State: '#FF0707',
+      Error_State: '#FF0707',
+      Secondary: '#FFEDD6',
+      Tertiary_color_1: '#FEE7E6',
+      Tertiary_color_2: '#F9EAC2',
+      Tertiary_color_3: '#ECFDF1',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
