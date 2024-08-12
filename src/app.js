b75f21e5afb04cc8a7b099ef952f9b2d2cc015fb
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-08-12 18:46:56 +0500

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 let login_popup = document.getElementById('login_popup');
 let signup_popup = document.getElementById('signup_popup');
+let login_eye = document.getElementById('login_eye');
+let input_login = document.querySelector('.input_login');
+
 
 function login_button() {
     login_popup.classList.add('!block');
@@ -15,4 +18,14 @@ function signup() {
 }
 function close_signup_popup() {
     signup_popup.classList.remove('!block');
+}
+
+function login_input_eye() {
+    if (input_login.type === 'password') {
+        login_eye.classList.add('fa-eye');
+        input_login.type = 'text';
+    }else{
+        login_eye.classList.remove('fa-eye');
+        input_login.type = 'password';
+    }
 }
